f7d238304b6d790f615a4fd6a4695163881e120f
Jason <st890670@gmail.com>
2022-07-27 01:11:34 +0800

    feat: add storybook
    1. import storybook
    2. change react default port

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -14,23 +14,5 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
   label: "Button",
 };
